fb29142b3a6f17c7decf4d956fccb14eef0c6bf7
Jamie Ferguson <jamie.ferguson@dijam.co.uk>
2015-12-19 14:39:43 +0000

    Update package file, add middleware to browser sync so subsequent page loads work

--- gulp/tasks/browser-sync.js
+++ gulp/tasks/browser-sync.js
@@ -4,13 +4,24 @@ import yargs       from 'yargs'
 import browserSync from 'browser-sync'
 import config      from '../config'
 import gulp        from 'gulp'
+import url         from 'url'
+import fs          from 'fs'
 
 gulp.task( 'browser-sync', function() {
-  browserSync( {
+  browserSync.init( {
     open: !!yargs.argv.open,
     notify: !!yargs.argv.notify,
     server: {
-      baseDir: config.destinations.root
+      baseDir: config.destinations.root,
+      middleware: function( req, res, next ) {
+        var fileName = url.parse( req.url )
+        fileName = fileName.href.split( fileName.search ).join( "" )
+        var fileExists = fs.existsSync( config.destinations.root + fileName )
+        if ( !fileExists && fileName.indexOf( 'browser-sync-client' ) < 0 ) {
+            req.url = '/index.html'
+        }
+        return next()
+      }
     }
   } )
 } )
