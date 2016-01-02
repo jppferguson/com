d0e74ac6491170e97d24fa1a4c5df223cc7ed3a9
Jamie Ferguson <jamie.ferguson@dijam.co.uk>
2016-01-02 21:01:56 +0000

    Add likeWhatYouSee directive

--- src/scripts/directives/index.js
+++ src/scripts/directives/index.js
@@ -3,6 +3,7 @@ import a               from './a'
 import backgroundImage from './backgroundImage'
 import elastic         from './elastic'
 import floatingLabel   from './floatingLabel'
+import likeWhatYouSee  from './likeWhatYouSee'
 import loading         from './loading'
 import readingTime     from './readingTime'
 import uiSrefActiveIf  from './uiSrefActiveIf'
@@ -13,6 +14,7 @@ var appDirectives = angular.module( 'appDirectives', [] )
   .directive( 'backgroundImage', backgroundImage )
   .directive( 'elastic',         elastic )
   .directive( 'floatingLabel',   floatingLabel )
+  .directive( 'likeWhatYouSee',  likeWhatYouSee )
   .directive( 'loading',         loading )
   .directive( 'readingTime',     readingTime )
   .directive( 'uiSrefActiveIf',  uiSrefActiveIf )
