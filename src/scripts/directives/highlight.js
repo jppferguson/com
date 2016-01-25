481acdaa526c9305adf0f5e64afcf8331713a0e0
Jamie Ferguson <jamie.ferguson@dijam.co.uk>
2016-01-25 00:46:34 +0000

    Fix highlight-js only working after revisiting the page

--- src/scripts/directives/highlight.js
+++ src/scripts/directives/highlight.js
@@ -1,18 +1,17 @@
 'use strict'
 import hljs from 'highlight.js'
 
-export default function( $timeout, $compile ) {
+export default function() {
   return {
     restrict: 'A',
     link: function( $scope, $elem ) {
-      $timeout( function() {
-        var items
-        $compile( $elem.contents() )( $scope )
-        items = $elem[0].querySelectorAll( 'code, pre' )
-        angular.forEach( items, function( item ) {
+      $scope.$watch( function() {
+        return $elem.text()
+      }, function() {
+        angular.forEach( $elem[0].querySelectorAll( 'code, pre' ), function( item ) {
           hljs.highlightBlock( item )
         } )
-      }, 500 )
+      } )
     }
   }
 }
